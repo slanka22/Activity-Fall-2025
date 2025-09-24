65536cfd444fde517e4e99e7e29c28b8cee4ed1e
Saeed Abdullah <me@saeedabdullah.com>
2025-09-23 20:25:55 -0400

    Activity 10 completed

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 // Add your JS code here
 
+for (let n = 1; n <= 20; n++) {
+    let output = "";
+    if ((n % 3 == 0) && (n % 5 == 0)) {
+        output="IST! HCDD!";
+    } else if (n % 3 == 0) {
+        output = "IST!";
+    } else if (n % 5 == 0) {
+        output = "HCDD!";
+    } else {
+        output = n;
+    }
+
+    console.log(output);
+}
