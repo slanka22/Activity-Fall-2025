fb5b7d4f57577f52b67e729c0742e2b2c6171385
Saeed Abdullah <me@saeedabdullah.com>
2025-09-25 13:46:55 -0400

    Starter files for Activity 11

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-// Add your JS code here
+const colleges = [
+    "College of Agricultural Sciences",
+    "College of Arts and Architecture",
+    "Smeal College of Business",
+    "Donald P. Bellisario College of Communications",
+    "College of Earth and Mineral Sciences",
+    "College of Education",
+    "College of Engineering",
+    "College of Health and Human Development",
+    "College of Information Sciences and Technology",
+    "Dickinson Law",
+    "Penn State Law",
+    "College of the Liberal Arts",
+    "College of Medicine",
+    "Ross and Carol Nese College of Nursing",
+    "Eberly College of Science",
+    "Schreyer Honors College",
+    "Graduate School",
+    "Pennsylvania College of Technology",
+];
 
-for (let n = 1; n <= 20; n++) {
-    let output = "";
-    if ((n % 3 === 0) && (n % 5 === 0)) {
-        output="IST! HCDD!";
-    } else if (n % 3 === 0) {
-        output = "IST!";
-    } else if (n % 5 === 0) {
-        output = "HCDD!";
-    } else {
-        output = n;
-    }
 
-    console.log(output);
-}
+// Add your code here
+// Random number
+// const randomNumber = Math.floor(Math.random() * colleges.length);
+// Update text in para
+// content.textContent = choice;
+
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", chooseCollege);
+const content = document.querySelector("#college");
