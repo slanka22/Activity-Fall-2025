7cc0d1f83bac44f4cc91b44785dae41251d84382
Saeed Abdullah <me@saeedabdullah.com>
2025-10-05 15:12:18 -0400

    Activity 16 starter files

--- main.js
+++ main.js
@@ -1,32 +1,10 @@
-const selectCollege = (colleges) => {
-    const randomNumber = Math.floor(Math.random() * colleges.length);
-    const choice = colleges[randomNumber];
-    console.log(choice.name);
-    content.textContent = choice.name;
+const addModule = (event) => {
+    const list = document.querySelector('ul');
+    const input = document.querySelector('input');
 
-}
-const fetchColleges = () => {
-    const url = "colleges.json"
-    const fetchPromise = fetch(url);
-    fetchPromise
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data[0].name);
-        })
-        .catch((error) => {
-            console.error(`Unable to fetch: ${error}`);
-        });
+    const moduleName = input.value;
 
+};
 
-}
-
-
-const btn = document.querySelector("button");
-btn.addEventListener("click", fetchColleges);
-const content = document.querySelector("#college");
-
+document.querySelector("#btn_add")
+    .addEventListener("click", addModule);
