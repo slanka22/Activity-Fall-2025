27b3d89852baf1b41e82feaf74e1da96b0b3ee2a
Saeed Abdullah <me@saeedabdullah.com>
2025-10-05 15:13:38 -0400

    Handles adding modules

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const addModule = (event) => {
 
     const moduleName = input.value;
 
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = moduleName;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = 'Delete';
+    listBtn.setAttribute("class", "tufte-button primary");
+    list.appendChild(listItem);
+
 };
 
 document.querySelector("#btn_add")
