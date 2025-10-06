1fb7e1e0ea3a24fa2ecfc44baf6a15631ad26a0e
Saeed Abdullah <me@saeedabdullah.com>
2025-10-06 13:52:12 -0400

    Updated starter files for activity 18

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 window.addEventListener("keydown", event => {
-    if (event.key == "v") {
+    if (event.key === "v") {
         console.log("keydown");
         document.body.style.background = "violet";
     }
 });
 window.addEventListener("keyup", event => {
-    if (event.key == "v") {
+    if (event.key === "v") {
         console.log("keyup");
         document.body.style.background = "";
     }
